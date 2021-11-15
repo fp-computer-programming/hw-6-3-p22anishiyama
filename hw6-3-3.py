60bed3f996e2d86323925aebfc94a9b96e07ca7e
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-11-15 17:41:13 -0500

    add hw6-3-3.py, hw6-3-4.py

--- hw6-3-3.py
+++ hw6-3-3.py
@@ -2,8 +2,9 @@
 
 numbers_in = input("Please enter five numbers: ")
 
-print(numbers_in)
-numbers_calculated = sum(list(numbers_in))
-print(numbers_in)
+numbers = list(numbers_in)
+numbers.sort()
+numbers[0:4] = []
 
-print("".format(numbers_in))
+total = int(numbers[0]) + int(numbers[1]) + int(numbers[2]) + int(numbers[3]) + int(numbers[4])
+print("The sum of these numbers is {0}.".format(total))
